506e7ceb5ef5a02fc23865d0cfe90e7c1644f5f5
Luckas Delboni <luckas12@gmail.com>
2023-01-13 17:06:46 -0300

    Search Bar

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,7 +1,20 @@
 import Header from '../../components/Header';
+import SearchBar from '../../components/SearchBar';
 import './styles.css'
 export default function Catalog() {
     return (
-        <Header />
+        <>
+            <Header />
+            <main className="ct-catalog-main">
+                <section id="ct-catalog-section">
+                    <div className="ct-search-bar-container">
+                    </div>
+                    <div className="ct-catalog-cards-container">
+                        <SearchBar />
+                    </div>
+                </section>
+
+            </main>
+        </>
     );
 }
