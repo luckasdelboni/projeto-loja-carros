c143f66f628609fae6712763ac4c6a79db402556
Luckas Delboni <luckas12@gmail.com>
2023-01-13 15:02:39 -0300

    Criação do Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css'
+import '../../assets/car.png'
+
+export default function Header() {
+    return (
+        <header>
+            <div className="ct-container ">
+                <h1> CARROS TOP </h1>
+            </div>
+        </header>
+    );
+}
---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,7 @@
+import Header from '../../components/Header';
+import './styles.css'
+export default function Catalog() {
+    return (
+        <Header />
+    );
+}
